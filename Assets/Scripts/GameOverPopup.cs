d10af4afaec951fbf99ab0dfd6d742e0f7a2c206
PrinzesschenPresswurst <prinzesschenpresswurst@googlemail.com>
2024-07-24 11:55:23 +0300

    added highscore handling

--- Assets/Scripts/GameOverPopup.cs
+++ Assets/Scripts/GameOverPopup.cs
@@ -1,12 +1,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOverPopup : MonoBehaviour
 {
     [SerializeField] private Canvas gameOverCanvas;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI highScoreText;
+    [SerializeField] private TextMeshProUGUI resultText;
+    
     
     private void Start()
     {
@@ -17,7 +22,16 @@ public class GameOverPopup : MonoBehaviour
     private void OnPlayerDied()
     {
         gameOverCanvas.gameObject.SetActive(true);
+        SetPopUpInfo();
         PlayerDeathHandler.PlayerDied -= OnPlayerDied;
+    }
+
+    private void SetPopUpInfo()
+    {
+        scoreText.text = "Score: " + ScoreKeeper.Score.ToString();
+        highScoreText.text = "HighScore: " + ScoreKeeper.HighScore.ToString();
+        if (ScoreKeeper.HighScoreWasBroken)
+            resultText.text = "You broke the HighScore!";
     }
     
     public void PlayAgain()
--- Assets/Scripts/ScoreKeeper.cs
+++ Assets/Scripts/ScoreKeeper.cs
@@ -7,30 +7,44 @@ using UnityEngine;
 public class ScoreKeeper : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI scoreText;
-    private int _score;
+    public static int Score { get; private set;}
+    public static int HighScore { get; private set;}
+    public static bool HighScoreWasBroken { get; private set;}
     private void Start()
     {
         Treat.TreatWasCollected += OnTreatWasCollected;
         PlayerDeathHandler.PlayerDied += OnPlayerDied;
-        _score = 0;
+        Score = 0;
+        HighScoreWasBroken = false;
         UpdateScore();
     }
 
     private void UpdateScore()
     {
-        scoreText.text = ""+ _score;
+        scoreText.text = ""+ Score;
     }
 
     private void OnTreatWasCollected()
     {
-        _score++;
+        Score++;
         UpdateScore();
     }
 
     private void OnPlayerDied()
     {
+        CheckForHighScore();
         Treat.TreatWasCollected -= OnTreatWasCollected;
         PlayerDeathHandler.PlayerDied -= OnPlayerDied;
     }
-    
+
+    private void CheckForHighScore()
+    {
+        HighScore = PlayerPrefs.GetInt("HighScore");
+        if (HighScore < Score)
+        {
+            PlayerPrefs.SetInt("HighScore", Score);
+            HighScoreWasBroken = true;
+            HighScore = Score;
+        }
+    }
 }
